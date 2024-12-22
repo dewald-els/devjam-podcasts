12187e4b4e433b022d93eea19fd51261c5dc71d8
Dewald Els <dewaldifels@gmail.com>
2024-12-22 18:06:22 +0100

    fix: Always load .env:

--- server/listen-notes/client.js
+++ server/listen-notes/client.js
@@ -1,4 +1,7 @@
 const { Client } = require("podcast-api");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 module.exports = Client({
 	apiKey: process.env.LISTEN_NOTES_API_KEY || undefined,
